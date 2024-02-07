f8dc5740f3e831202eb64ee89fbbede094d88fd4
HelgagleH <olyyyya@ukr.net>
2024-02-07 18:44:53 +0200

    Created project

---
+++ components/Profil/Profil.jsx
@@ -0,0 +1,12 @@
+import { TouchableOpacity, Text } from "react-native";
+
+export function Profil() {
+    return (
+        <>
+            <Text>Hi I'm Olha</Text>
+            <TouchableOpacity>
+                <Text>Click me</Text>
+            </TouchableOpacity>
+        </>
+    );
+}
